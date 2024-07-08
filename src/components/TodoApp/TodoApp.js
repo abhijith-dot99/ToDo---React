4dd89472de62affd5f17b5620bf418e7f8a91fd3
abhijith-99 <abhijith@au-ki.com>
2024-07-08 13:02:41 +0530

    changes after deploy

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -4,35 +4,55 @@ class TodoApp extends Component {
 
     state= {
         input: "",
+        items: []
     };
 
     handleChange = (event) => {
-     this.setState( { secondvalue: event.target.value}, () => {        
+     this.setState( { input: event.target.value}, () => {        
      });
-     console.log(this.state.secondvalue);
+     console.log(this.state.input)
     };
 
+    storeItems = (event) => {
+        event.preventDefault();
+        const {input} = this.state;
+
+        if (input.trim() === "") {            
+            return;
+        }
+
+        this.setState({
+            items: [...this.state.items, input],
+            input: ""
+        })
+    }
+
+    handleDelete = (items) => {
+        const allItems = this.state.items;
+        allItems.splice(items, 1);
+
+        this.setState({
+            items: allItems
+        })
+        console.log(this.items);
+
+    }
+
     render() {
-        const { newvalue } = this.state;
+        const { input , items } = this.state;
+        console.log(items)
         return (
             <div className="todo-container">
-                <form className="input-section">
+                <form className="input-section" onSubmit={this.storeItems}>
                     <h1>TodoApp</h1>
-                    <input type="text" value={newvalue} onChange={this.handleChange} placeholder="Enter data..."></input>
+                    <input type="text" value={input} onChange={this.handleChange} placeholder="Enter data..."></input>
                 </form>
-                <ul className="ul-items">
-                    <li>
-                        items <i className="fa-solid fa-xmark"></i>
-                    </li>
-                    <li>
-                        items <i className="fa-solid fa-xmark"></i>
-                    </li>
-                    <li>
-                        items <i className="fa-solid fa-xmark"></i>
-                    </li>
-                    <li>
-                        items <i className="fa-solid fa-xmark"></i>
-                    </li>
+                <ul className="ul-items">                    
+                    {items.map((data, index) => (
+                        <li key={index}>
+                            {data} <i className="fa-solid fa-xmark" onClick={ () => this.handleDelete(index)}></i>
+                        </li>
+                    ))}                                 
                 </ul>
             </div>
         );
