062ea8a80411def38ff5de716bf679811abec53c
abhijith-99 <abhijith@au-ki.com>
2024-07-16 23:07:09 +0530

    changes in header

--- src/components/TodoApp/Header/Header.js
+++ src/components/TodoApp/Header/Header.js
@@ -1,13 +1,17 @@
-
-import React from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <div className='header-tag'> 
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
-</div>
-  )
+    return (
+        <div className="header-tag">
+            <div className="logo-container">
+                <img src={`${process.env.PUBLIC_URL}/logo.jfif`} className="logo" alt="" />
+            </div>
+            <div className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </div>
+    );
 }
